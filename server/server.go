671460faa58e56d8d0f22b5e5643ce1232899818
SJ-KR <djtm99@naver.com>
2020-05-24 20:06:16 -0700

    update server (added a new type: StubPlayerStore{})

--- server/server.go
+++ server/server.go
@@ -6,17 +6,42 @@ import (
 	"strings"
 )
 
-func GetPlayerScore(name string) string {
+type PlayerStore interface {
+	GetPlayerScore(name string) int
+}
+
+type PlayerServer struct {
+	store PlayerStore
+}
+
+func (p *PlayerServer) GetPlayerScore(name string) int {
 	if name == "Pepper" {
-		return "20"
+		return 20
 	}
 	if name == "Floyd" {
-		return "10"
+		return 10
 	}
-	return ""
+	return 0
 }
+
+func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+	fmt.Fprintf(w, "%d", p.store.GetPlayerScore(player))
+}
+
+/*
 func PlayerServer(w http.ResponseWriter, r *http.Request) {
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
 
 	fmt.Fprint(w, GetPlayerScore(player))
+}
+*/
+
+type StubPlayerStore struct {
+	scores map[string]int
+}
+
+func (s *StubPlayerStore) GetPlayerScore(name string) int {
+	score := s.scores[name]
+	return score
 }
--- server/server_test.go
+++ server/server_test.go
@@ -8,31 +8,39 @@ import (
 )
 
 func TestGETPlayers(t *testing.T) {
+	store := StubPlayerStore{
+		map[string]int{
+			"Pepper": 20,
+			"Floyd":  10,
+		},
+	}
+	server := &PlayerServer{&store}
+
 	t.Run("returns Pepper's score", func(t *testing.T) {
 		request := newGetScoreRequest("Pepper")
 		response := httptest.NewRecorder()
 
-		PlayerServer(response, request)
+		server.ServeHTTP(response, request)
 
-		assertResponseBody(t, response, "20")
+		assertResponseBody(t, response.Body.String(), "20")
 	})
+
 	t.Run("returns Floyd's score", func(t *testing.T) {
 
 		request := newGetScoreRequest("Floyd")
 		response := httptest.NewRecorder()
 
-		PlayerServer(response, request)
+		server.ServeHTTP(response, request)
 
-		assertResponseBody(t, response, "10")
+		assertResponseBody(t, response.Body.String(), "10")
 	})
 }
+
 func newGetScoreRequest(name string) *http.Request {
 	req, _ := http.NewRequest(http.MethodGet, fmt.Sprint("/players/", name), nil)
 	return req
 }
-func assertResponseBody(t *testing.T, response *httptest.ResponseRecorder, want string) {
-
-	got := response.Body.String()
+func assertResponseBody(t *testing.T, got, want string) {
 
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
