ecf7eb1124f4aa49acdde263fa1ba2d086229460
SJ-KR <djtm99@naver.com>
2020-05-26 02:21:28 -0700

    update server (updated a missing player scenario)

--- server/server.go
+++ server/server.go
@@ -30,8 +30,11 @@ func (p *PlayerServer) GetPlayerScore(name string) int {
 
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
+	score := p.store.GetPlayerScore(player)
 
-	w.WriteHeader(http.StatusNotFound)
+	if score == 0 {
+		w.WriteHeader(http.StatusNotFound)
+	}
 
 	fmt.Fprintf(w, "%d", p.store.GetPlayerScore(player))
 }
--- server/server_test.go
+++ server/server_test.go
@@ -19,9 +19,9 @@ func TestGETPlayers(t *testing.T) {
 	t.Run("returns Pepper's score", func(t *testing.T) {
 		request := newGetScoreRequest("Pepper")
 		response := httptest.NewRecorder()
-
 		server.ServeHTTP(response, request)
 
+		assertStatus(t, response.Code, http.StatusOK)
 		assertResponseBody(t, response.Body.String(), "20")
 	})
 
@@ -29,9 +29,9 @@ func TestGETPlayers(t *testing.T) {
 
 		request := newGetScoreRequest("Floyd")
 		response := httptest.NewRecorder()
-
 		server.ServeHTTP(response, request)
 
+		assertStatus(t, response.Code, http.StatusOK)
 		assertResponseBody(t, response.Body.String(), "10")
 	})
 	t.Run("returns 404 on missing players", func(t *testing.T) {
@@ -40,12 +40,7 @@ func TestGETPlayers(t *testing.T) {
 
 		server.ServeHTTP(response, request)
 
-		got := response.Code
-		want := http.StatusNotFound
-
-		if got != want {
-			t.Errorf("got status %d want %d", got, want)
-		}
+		assertStatus(t, response.Code, http.StatusNotFound)
 	})
 }
 
@@ -56,6 +51,13 @@ func newGetScoreRequest(name string) *http.Request {
 func assertResponseBody(t *testing.T, got, want string) {
 
 	if got != want {
-		t.Errorf("got %q, want %q", got, want)
+		t.Errorf("body is wrong, got %q, want %q", got, want)
+	}
+}
+func assertStatus(t *testing.T, got, want int) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("did not get correct status, got %d, want %d", got, want)
 	}
 }
